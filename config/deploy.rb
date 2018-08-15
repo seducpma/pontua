662a54c1e16ccac8ad63e4205752ad8326ee8691
seducpma <naor@seducpma.com>
2018-08-15 11:52:05 -0300

    2018-08-15-1152

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,8 @@
 #set :application, "192.168.0.12"
 set :application, "170.36.159.102" #170.81.237.114 - Servidor oficial (hera)  # 170.36.159.102 - Servidor Teste Alvaro/Alexandre
 set :repository, "git://github.com/seducpma/pontua.git"
-set :user, "servidor"
-#set :user, "hera"
+#set :user, "servidor"
+set :user, "hera"
 set :use_sudo, false
 set :deploy_to, "/home/#{user}/pontua.seducpma.com"
 set :scm, :git
